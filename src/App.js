27a3d5c9aadc75946f26fe097c3957c006e5c020
Wesley Freeman <22487192+wesfree163@users.noreply.github.com>
2021-07-05 02:10:52 -0400

    A list of malicious intent via machines.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import roboto from './imagery/roboto.jpg';
+import React, { Component } from 'react';
+import Special from './components/Special'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App" className="App-header">
+      <Router>
+        <Switch>
+          <Route path="/Special" component={Special} exact/>
+        </Switch>
+      </Router>
+      <header>
+        <a href="/"><img src={roboto}/></a>
+        
+        
       </header>
+      <h1>Cyber Beastiary</h1>
+      <h2>A collection of dangers on the World Wide Web</h2>
+      <ul>
+        <li>Client-Side Vulnerabilities</li>
+        <li>Server-Side Vulnerabilities</li>
+        <li><a href="/Special">Special Cases</a></li>
+      </ul>
     </div>
   );
 }
